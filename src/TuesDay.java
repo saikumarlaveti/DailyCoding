4feaa47bc19d74083ed89c62f947c9d4b23534d4
saikumar <saikumarlaveti5061@gmail.com>
2024-08-10 20:16:16 +0530

    final

--- src/TuesDay.java
+++ src/TuesDay.java
@@ -3,7 +3,7 @@ public class TuesDay
 {
     public static void main(String[] args)
     {
-        int n = 10;
+        int n = 20;
         for(int i = 2;i<=n;i++){
             int count=0;
             for(int j =2;j<i;j++){
